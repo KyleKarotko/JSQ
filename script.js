0f435c34eaf1b4d272c2ddce700958465d28ee6c
kyle_karotko <kkrtk7@duck.com>
2022-11-15 23:15:14 -0500

    moved highscores to main page

--- script.js
+++ script.js
@@ -34,6 +34,7 @@ var questions = [
 var secondsLeft = 75;
 var questionList = 0;
 var quizEl = document.getElementById("questions")
+var feedbackEl = document.getElementById('feedback');
 var endScreenEl = document.getElementById('end-screen');
 var timerEl = document.getElementById('time');
 var initialsEl = document.getElementById('initials');
@@ -77,9 +78,9 @@ function askQuestion(){
       button.setAttribute("value",choice)
       button.onclick = function(){
         if (this.value !== currentQuestion.answer){
-          console.log("wrong")
+          feedbackEl.textContent = 'Wrong!';
         } else {
-          console.log("correct")
+          feedbackEl.textContent = 'Correct!';
         }
         questionList++;
         if (questionList===questions.length) {
@@ -95,27 +96,30 @@ function askQuestion(){
     })
 
 }
+
+// save scores
 submitBtn.onclick = saveScore;
 function saveScore() {
   var initials = initialsEl.value;
-
-  // make sure value wasn't empty
-  //if (initials !== '') {
-    // get saved scores from localstorage, or if not any, set to empty array
-    var currentScore =
-      JSON.parse(window.localStorage.getItem('highscores')) || [];
-
-    // format new score object for current user
+   
+    var currentScore = JSON.parse(window.localStorage.getItem('#highscores'));
     var newScore = {
       score: time,
       initials: initials,
     };
-
-    // save to localstorage
     currentScore.push(newScore);
-    window.localStorage.setItem('highscores', JSON.stringify(currentScore));
+    window.localStorage.setItem('#highscores', JSON.stringify(currentScore));
 
-    // redirect to next page
-    window.location.href = 'highscores.html';
+ 
   }
-//}
+    // display highscores
+    
+  function showScores(){
+    var currentScore = JSON.parse(window.localStorage.getItem('highscores'));
+    for (var i = 0; i < currentScore.length; i += 1) {
+      var liEl= document.createElement('li');
+      liEl.textContent = currentScore[i].initials + ' - ' + currentScore[i].score;
+      var olEl = document.getElementById('highscores');
+      olEl.appendChild(liTag);
+  }
+};
