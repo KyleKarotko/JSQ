bfd596f28818e63cb0e3afac17320c235ba5ba72
kyle_karotko <kkrtk7@duck.com>
2022-11-15 20:37:00 -0500

    updated save score

--- script.js
+++ script.js
@@ -36,8 +36,10 @@ var questionList = 0;
 var quizEl = document.getElementById("questions")
 var endScreenEl = document.getElementById('end-screen');
 var timerEl = document.getElementById('time');
+var initialsEl = document.getElementById('initials');
 //var timerId;
 var time = questions.length * 15;
+var submitBtn = document.getElementById('submit');
 
 
 // starts the quiz
@@ -93,4 +95,27 @@ function askQuestion(){
     })
 
 }
+submitBtn.onclick = saveScore;
+function saveScore() {
+  var initials = initialsEl.value;
 
+  // make sure value wasn't empty
+  //if (initials !== '') {
+    // get saved scores from localstorage, or if not any, set to empty array
+    var currentScore =
+      JSON.parse(window.localStorage.getItem('highscores')) || [];
+
+    // format new score object for current user
+    var newScore = {
+      score: time,
+      initials: initials,
+    };
+
+    // save to localstorage
+    currentScore.push(newScore);
+    window.localStorage.setItem('highscores', JSON.stringify(currentScore));
+
+    // redirect to next page
+    window.location.href = 'highscores.html';
+  }
+//}
